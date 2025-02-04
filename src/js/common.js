95e50f7300bd122931072ca185361454cadf0ce0
Anna Ulanova <Zhirnova-9797@mail.ru>
2025-02-04 17:03:29 +0300

    init

---
+++ src/js/common.js
@@ -0,0 +1,53 @@
+import './helpers/globalFunctions.js';
+import BaseModal from './components/modals/BaseModal.js';
+import Tooltip from "./components/common/Tooltip.js";
+
+// info: Инициализация глобальных для приложения компонентов, функций или событий
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    // info: Если находимся в режиме локальной разработки,
+    //  отображаем в углу виджет со списком страниц для удобной навигации между ними.
+    if (window?.APP?.mode === 'development') {
+        const pages = [
+            'index',
+            'catalog',
+        ];
+
+        import('./helpers/pagesWidget.js').then(module => {
+            if (module) {
+                module.default(pages);
+            }
+        });
+    }
+
+    // info: Функция для получения файла svg-спрайта и его вставки в DOM.
+    if (window?.APP?.svgSpritePath) {
+        (async () => {
+            try {
+                const response = await fetch(window.APP.svgSpritePath);
+                const sprite = await response.text();
+                const SVGContainer = document.getElementById('__SVG-container__');
+
+                if (sprite || !response) {
+                    SVGContainer.innerHTML = sprite;
+                    SVGContainer.querySelector('style')
+                        ?.remove();
+                } else {
+                    new Error('sprite loading error');
+                }
+            } catch (error) {
+                throw error;
+            }
+        })();
+    }
+
+    document.querySelectorAll('.js-open-modal').forEach((modal) => {
+        modal.addEventListener('click', BaseModal.showModal.bind(this, modal.dataset.modalId, {}))
+    })
+
+    document.querySelectorAll('.js-tooltip').forEach((element) => new Tooltip({
+        element: element,
+        showOnClick: true,
+        placement: 'bottom'
+    }))
+});
