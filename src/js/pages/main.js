e9bdd18de3aab2d3622f67200bf46605ee15b4dd
Anna Ulanova <Zhirnova-9797@mail.ru>
2025-02-09 19:59:09 +0300

    add: adaptive

---
+++ src/js/pages/main.js
@@ -0,0 +1,20 @@
+let clientsArr = [
+  {
+    surname: "Скворцов",
+    name: "Денис",
+    middleName: "Юрьевич",
+    contsctArr,
+  },
+];
+let contsctArr = [
+  {
+    type: "",
+    value: "",
+  },
+];
+
+console.log(clientsArr);
+// phone: "",
+// vk: "",
+// fb: "",
+// mail: "",
